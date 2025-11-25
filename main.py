4e31b2378e75a6dc3d1eff710f94dede6c5c040b
Seyali-ahm <seyyedaliahmadi9@gmail.com>
2025-11-25 13:13:33 +0100

    Add Sqlie connection and translation tables

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI(title="RAG Translation Backend")
 
+conn = sqlite3.connect("rag.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pairs(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        source_lang TEXT,
+        target_lang TEXT,
+        sentence TEXT,
+        translation TEXT
+    )
+""")
+conn.commit()
+
 @app.get("/")
 def root():
-    return {"status": "running"}
+    return {"db": "initialized"}
